9dee0f98119305354938d8608cfae14c5d944ad3
J Richard <jrichard.connect@gmail.com>
2024-10-08 10:41:56 +0700

    Import module to use on main file

--- NodeJS/myfirst.js
+++ NodeJS/myfirst.js
@@ -1,8 +1,10 @@
 var http = require('http');
+var dt = require('./myfirstmodule');
 
 http.createServer(function (req, res) {
     // writeHead is the method to set the HTTP status code
     res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("The date and time are currently: " + dt.myDateTime());
     // This method sends the response body and signal that the response is complete
-    res.end('Hello World!');
+    res.end();
 }).listen(8080); // This is the port definition, localhost:8080
