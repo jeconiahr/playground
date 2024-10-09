d8a603e504b08f3d1c21e6dbff000098272de956
J Richard <jrichard.connect@gmail.com>
2024-10-09 11:10:25 +0700

    Established db Connection

---
+++ NodeJS/ExpenseTracker/backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const { db } = require('./db/db')
+
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+const server = () => {
+    db()
+    app.listen(PORT, () => {
+        console.log("Listening to port", PORT);
+    })
+}
+
+server();
