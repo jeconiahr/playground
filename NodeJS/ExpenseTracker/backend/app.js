2acce41382b2b79c8c8614b260bbc4bce53e0b78
J Richard <jrichard.connect@gmail.com>
2024-10-09 13:01:22 +0700

    Added Routes to App with FS

--- NodeJS/ExpenseTracker/backend/app.js
+++ NodeJS/ExpenseTracker/backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { db } = require('./db/db')
+const {readdirSync} = require('fs');
 
 const app = express();
 require('dotenv').config();
@@ -10,6 +11,9 @@ const PORT = process.env.PORT
 // Middlewares
 app.use(express.json());
 app.use(cors());
+
+// Routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
 app.get('/', (req, res) => {
     res.send('Hello World')
